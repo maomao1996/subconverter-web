a7803c50ef174fa79c3d68094bc76ea5da40a814
maomao <1714487678@qq.com>
2025-04-07 11:07:22 +0800

    fix: 修复 github pages 部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
+import { basename } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+
+const APP_BASE_PATH = basename(process.env.APP_BASE_PATH || '')
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
 })
